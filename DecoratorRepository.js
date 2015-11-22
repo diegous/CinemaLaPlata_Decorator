412fe55229d26db789745ac41acecf2241f99394
diegous <munoa.d@gmail.com>
2015-11-22 11:53:35 -0300

    Todos los archivos nuevos

---
+++ DecoratorRepository.js
@@ -0,0 +1,27 @@
+movie_decorators = {
+    {
+        "type" : "movie",
+        "elements" : 
+        [
+            "IMDB" : {
+                "name" : "IMDB",
+                "functions" : {
+                    getRanking : function () {
+
+                    },
+                    getDirectors : function () {},
+                    .
+                    .
+                    .
+                    N
+                }
+            },
+            "MERCA" : {
+                "name" : "Mercado Libre"
+            }
+        ]
+    }
+    
+}
+
+song_decorators = 
