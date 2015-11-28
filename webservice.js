70fdf5d383b0e62be8220653d6fdf70750521dd7
diegous <munoa.d@gmail.com>
2015-11-27 21:10:45 -0300

    Anda la primera mitad del programa

---
+++ webservice.js
@@ -0,0 +1,42 @@
+[
+    {
+        "name": "Cartelera de Cinema La Plata",
+        "urlPattern": "http://cinemalaplata.com/cartelera.aspx",
+        "concepts": [
+            {
+                "id": 1440162653658,
+                "name": "Movie",
+                "tags": ["movie"],
+                "xpath": "//div[@class=\"page-container singlepost\"]",
+                "properties": [
+                    {
+                        "id": 1440000442392,
+                        "name": "Title",
+                        "tags": ["title"],
+                        "xpath": ".//h4[@class=\"shortcodes-title\""
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Sitio de prueba",
+        "urlPattern": "file:///D:/oo2/CinemaLaPlata_Decorator/TestMenus.html",
+        "concepts": [
+            {
+                "id": 432432432,
+                "name": "Pelicula",
+                "tags": ["movie", "Peli"],
+                "xpath": "//li[@class=\"peli\"]",
+                "properties": [
+                    {
+                        "id": 43432,
+                        "name": "Title",
+                        "tags": ["title"],
+                        "xpath": "//p[@class=\"id\"]"
+                    }
+                ]
+            }
+        ]
+    },
+]
