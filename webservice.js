4339b599ac4de13a135d9e4d66ec0ba70f932404
Enrique <diego240@gmail.com>
2015-12-25 20:59:59 -0300

    Se incluye en webservice.js meores paths

--- webservice.js
+++ webservice.js
@@ -1,19 +1,53 @@
 var webserviceData = [
         {
             "name": "Cartelera de Cinema La Plata",
-            "urlPattern": "http://cinemalaplata.com/cartelera.aspx",
+            "urlPattern": "http://www.cinemalaplata.com/Cartelera.aspx",
             "concepts": [
                 {
                     "id": 1440162653658,
                     "name": "Movie",
                     "tags": ["movie"],
                     "xpath": "//div[@class=\"page-container singlepost\"]",
+                    "mainProperty": "0",
                     "properties": [
+                        {
+                            "id": 1440000442391,
+                            "name": "Menu Location",
+                            "tags": ["menu_location"],
+                            "xpath": ".//h4[@class=\"shortcodes-title\"]"
+                        },
                         {
                             "id": 1440000442392,
                             "name": "Title",
                             "tags": ["title"],
-                            "xpath": ".//h4[@class=\"shortcodes-title\""
+                            "xpath": ".//h4[@class=\"shortcodes-title\"]/strong/a/text()"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Cartelera de Cinema La Plata",
+            "urlPattern": "file:///home/enrique/git/CinemaLaPlata_Decorator/page.html",
+            "concepts": [
+                {
+                    "id": 1440162653658,
+                    "name": "Movie",
+                    "tags": ["movie"],
+                    "xpath": "//div[@class=\"page-container singlepost\"]",
+                    "mainProperty": "0",
+                    "properties": [
+                        {
+                            "id": 1440000442391,
+                            "name": "Menu Location",
+                            "tags": ["menu_location"],
+                            "xpath": ".//h4[@class=\"shortcodes-title\"]"
+                        },
+                        {
+                            "id": 1440000442392,
+                            "name": "Title",
+                            "tags": ["title"],
+                            "xpath": ".//h4[@class=\"shortcodes-title\"]/strong/a/text()"
                         }
                     ]
                 }
@@ -21,13 +55,14 @@ var webserviceData = [
         },
         {
             "name": "Sitio de prueba",
-            "urlPattern": "file:///D:/oo2/CinemaLaPlata_Decorator/TestMenus.html",
+            "urlPattern": "file:///home/enrique/git/CinemaLaPlata_Decorator/TestMenus2_sin_javascript.html",
             "concepts": [
                 {
                     "id": 432432432,
                     "name": "Pelicula",
                     "tags": ["movie", "Peli"],
                     "xpath": "//li[@class=\"peli\"]",
+                    "mainProperty": "0",
                     "properties": [
                         {
                             "id": 43432,
